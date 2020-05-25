a43ed2c225262b0cc695d9287e42ab0bde59ff59
marcelosz <marcelosz@git>
2020-05-25 15:35:30 -0300

    Update mwgcs-logger.py

--- mwgcs-logger.py
+++ mwgcs-logger.py
@@ -10,10 +10,12 @@ __license__ = "GPL"
 
 import sys, logging, argparse, textwrap
 import time
-import cgi
-import StringIO
 import requests
 from requests.auth import HTTPBasicAuth
+import StringIO
+import cgi
+import csv
+import re
 
 # Enable logging for the script itself
 log_formatter = logging.Formatter('%(asctime)s mwgcs-logger (%(name)s) %(levelname)s: %(message)s')
@@ -92,10 +94,13 @@ def main(argv):
 
     # build the filter, starting by the timestamps
     mwgcsFilter = "filter.requestTimestampFrom=" + str(requestTimestampFrom) + "&amp;filter.requestTimestampTo=" + str(requestTimestampTo)
+    enabledFilters = 0
     if args.userfilter:
+        enabledFilters += 1
         escapedUserFilter = cgi.escape("\"" + args.userfilter + "\"")
         mwgcsFilter += "&amp;filter.userName=" + str(escapedUserFilter)
     if args.resultfilter:
+        enabledFilters += 1        
         mwgcsFilter += "&amp;filter.result=" + str(args.resultfilter)
     mwgcsFilter += "&amp;order.0.requestTimestamp=asc"   
     mwgcsURL = 'https://' + conf_util.cfg['MWGCS']['Host'] + '/mwg/api/reporting/forensic/' + conf_util.cfg['MWGCS']['CustomerID'] + "?" + mwgcsFilter
@@ -128,10 +133,28 @@ def main(argv):
             logger.error("Invalid first line from response: " + responseLines[0])
 
         # print the collected logs - for some reason there are 2 empty lines, plus the header (that's why the number 3 here)
-        logger.info("Now printing " + str(nLines - 3) + " log entries collected...")
-        for line in responseLines:
-            print line
-            
+        logger.info("Total number of log entries collected: " + str(nLines - 3))
+        logger.info("Now printing logs that match filter parameters (if any)...")
+        logger.info("Header: " + csvHeader)
+        #print '"request_timestamp" "username" "source_ip" "http_action" "requested_host" "requested_path" "category" "reputation" "last_rule" "result" "block_reason" "process_name" "destination_ip" "destination_port"'
+
+        csvDict = csv.DictReader(responseLines)
+        logLines = responseLines[1:]
+        nRow = 0
+        for row, line in zip(csvDict, logLines):
+            filterMatch = 0
+            if args.userfilter:
+                if (re.search(args.userfilter, row['username'])):
+                    filterMatch += 1
+            if args.resultfilter:
+                if (re.search(args.resultfilter, row['result'])):
+                    filterMatch += 1
+            if not enabledFilters or enabledFilters == filterMatch :
+                nRow += 1
+                print line
+                #print str(nRow) + ": " + row['request_timestamp'], row ['username'], row['source_ip'], row['http_action'], row['requested_host'], row['requested_path'], \
+                #row['category'], row['reputation'], row['last_rule'], row['result'], row['block_reason'], row['process_name'], row ['destination_ip'], row['destination_port']
+           
     except Exception as e:
         logger.error(str(e))
         exit(1)
